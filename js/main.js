a8424f55ced944d8fafbe69f92b866c19dace70a
caiyin <763416559@qq.com>
2020-08-05 15:46:27 +0800

    feat: 更新 Demo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
-(function () {
-  window.onload = function () {
-    /**
-     * 配置 MutationObserver
-     */
-    const observerCallback = function (mutationList, observer) {
-      console.log(mutationList, observer);
-    };
-    const observer = new MutationObserver(observerCallback);
+/**
+ * 创建 observer 对象
+ */
+const observerCallback = function (mutationList, observer) {
+  console.log('Observer: ', observer);
 
-    /**
-     * 元素监测的参数
-     */
-    const observerConfig = {
-      childList: true, // 观察目标子节点的变化，添加或者删除
-      attributes: true, // 观察属性变动
-      subtree: true, // 默认为 false，设置为 true 可以观察后代节点
-    };
+  mutationList.forEach((mutationRecord) => {
+    console.log(mutationRecord);
+  });
+};
+const observer = new MutationObserver(observerCallback);
 
-    /**
-     * 带监测的 DOM 元素
-     */
-    const targetElement = document.getElementById('line');
+/**
+ * 监测 DOM 元素
+ */
+const targetElement = document.querySelector('ul');
+const observerConfig = {
+  childList: true, // 观察目标子节点的变化，添加或者删除
+  attributes: true, // 观察属性变动
+  subtree: true, // 默认为 false，设置为 true 可以观察后代节点（默认只观察父节点，即 observe 的节点本身）
+};
+observer.observe(targetElement, observerConfig);
 
-    /**
-     * 监测 DOM 变化
-     */
-    observer.observe(targetElement, observerConfig);
-  };
-})();
+/**
+ * 停止监听
+ */
+// observer.disconnect();
+
+/**
+ * 一些辅助操作
+ */
+// childList 属性变化
+function addList() {
+  const childLen = targetElement.childElementCount;
+
+  const newLi = document.createElement('li');
+  newLi.innerHTML = `This is a new Paragraph, the line is ${childLen}`;
+
+  targetElement.appendChild(newLi);
+}
+// attribute 属性变化
+function changeColor() {
+  const firstChild = targetElement.firstElementChild;
+  firstChild.setAttribute('style', 'color: #5392ff');
+}
